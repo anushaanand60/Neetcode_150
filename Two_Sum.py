08fe759a1063ad982cbfa1c7d39e9455884cb746
Anusha Anand <anandanusha610@gmail.com>
2025-06-22 05:11:48 +0530

    Q1

--- Two_Sum.py
+++ Two_Sum.py
@@ -5,31 +5,19 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        pair_idx = {}  # Dictionary to store number:index pairs
-
+        pair_idx = {}
         for i, num in enumerate(nums):
-            complement = target - num  # The number we need to find
-
+            complement = target - num
             if complement in pair_idx:
-                # If complement exists, return its index and current index
                 return [pair_idx[complement], i]
-
-            # Store the current number and its index in the dictionary
             pair_idx[num] = i
 
-        # If no solution is found
         return []
 
-
-# Main program with user input
 if __name__ == "__main__":
-    # Take a list of integers as input
     nums_input = input("Enter numbers separated by spaces: ")
     nums = list(map(int, nums_input.strip().split()))
-
-    # Take the target value as input
     target = int(input("Enter the target number: "))
-
     solution = Solution()
     result = solution.twoSum(nums, target)
 
